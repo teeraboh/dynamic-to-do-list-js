aa2c922660a3b48fd03d8ca725d7b1f766baeb57
Fidelis Zana Teeraboh <teeraboh@gmail.com>
2025-01-26 17:51:07 +0000

    updating the js file to do list

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const removeButton = document.createElement('button');
     removeButton.textContent = 'Remove';
-    removeButton.className = 'remove-btn';
+    removeButton.classList.add('remove-btn');
 
     removeButton.onclick = function () {
       taskList.removeChild(taskItem);
